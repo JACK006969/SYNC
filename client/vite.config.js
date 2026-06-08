506c326604bfc5106b7dd8b5d67f869bfbaa0089
JACK006969 <KUTKUTHODOL23@GMAIL.COM>
2026-06-08 15:35:14 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  root: '.', // Tell Vite that client folder is the root
+  base: '/',
+  build: {
+    outDir: '../server/dist', // Build output goes to server/dist
+    emptyOutDir: true
+  },
   server: {
     proxy: {
       '/socket.io': { target: 'http://localhost:3000', ws: true }
